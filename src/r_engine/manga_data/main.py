f47b7ef33949cb77a06b7aa48519bd2e5817cd3a
valiantlynx <gormerykombo@gmail.com>
2024-04-18 22:01:57 +0000

    fuck it works

--- src/r_engine/manga_data/main.py
+++ src/r_engine/manga_data/main.py
@@ -1,20 +1,27 @@
 import pandas as pd
-from embed_data_api import MangaVectorizer, MangaDatabase
+from .embed_data_api import MangaVectorizer, MangaDatabase
 import os
 
-file_dir = os.path.dirname(os.path.realpath(__file__))
+def manga_recommendations(title, size):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    manga_data = pd.read_csv(file_dir + '/files/mangas_embedd.csv')
+    manga_data.drop_duplicates(subset=['title'], inplace=True)
+    manga_data.fillna('', inplace=True)
 
+    vectorizer = MangaVectorizer(manga_data)
+    vectorized_mangas = vectorizer.vectorize_mangas(['title', 'description', 'authors', 'genres', 'rating', 'views', 'latestChapter', 'lastUpdated'])
 
-manga_data = pd.read_csv(file_dir + '/files/mangas_embedd.csv')
-manga_data.drop_duplicates(subset=['title'], inplace=True)
-manga_data.fillna('', inplace=True)
+    db_manager = MangaDatabase(file_dir + "/data/manga-db")
+    table = db_manager.create_table("manga_set", data=vectorized_mangas.to_dict(orient='records'))
 
-vectorizer = MangaVectorizer(manga_data)
-vectorized_mangas = vectorizer.vectorize_mangas(['title', 'description', 'authors', 'genres', 'rating', 'views', 'latestChapter', 'lastUpdated'])
+    query_vector = vectorized_mangas.loc[vectorized_mangas['title'] == title, 'vector'].iloc[0]
+    result = db_manager.search("manga_set", query_vector, limit=size)
+    
+    # Construct return with title and manga ID sorted by distance
+    sorted_results = result.sort_values(by='_distance')  # Sort by distance
+    return_list = [{'title': row['title'], 'manga_id': row['id']} for _, row in sorted_results.iterrows()]
 
-db_manager = MangaDatabase(file_dir + "/data/manga-db")
-table = db_manager.create_table("manga_set", data=vectorized_mangas.to_dict(orient='records'))
+    return return_list
+    
+manga_recommendations(title="Rebirth Of The Immortal Venerable", size=10)
 
-query_vector = vectorized_mangas.loc[vectorized_mangas['title'] == "Rebirth Of The Immortal Venerable", 'vector'].iloc[0]
-result = db_manager.search("manga_set", query_vector, limit=20)
-print(result)
--- src/r_engine/manga_data/test.py
+++ src/r_engine/manga_data/test.py
@@ -1,49 +1,28 @@
-import lancedb
-
-import numpy as np
 import pandas as pd
-from main import get_recommendations, data
-import main
+from embed_data_api import MangaVectorizer, MangaDatabase
+import os
 
 # TESTING ======================================================
+def test_main(title, size):
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    manga_data = pd.read_csv(file_dir + '/files/mangas_embedd.csv')
+    manga_data.drop_duplicates(subset=['title'], inplace=True)
+    manga_data.fillna('', inplace=True)
 
+    vectorizer = MangaVectorizer(manga_data)
+    vectorized_mangas = vectorizer.vectorize_mangas(['title', 'description', 'authors', 'genres', 'rating', 'views', 'latestChapter', 'lastUpdated'])
 
-def test_main():
-    ratings = pd.read_csv('./manga-data/ratings.csv', header=None,
-                          names=["user id", "manga id", "rating", "timestamp"])
-    ratings = ratings.drop(columns=['timestamp'])
-    ratings = ratings.drop(0)
-    ratings["rating"] = ratings["rating"].values.astype(np.float32)
-    ratings["user id"] = ratings["user id"].values.astype(np.int32)
-    ratings["manga id"] = ratings["manga id"].values.astype(np.int32)
+    db_manager = MangaDatabase(file_dir + "/data/manga-db")
+    table = db_manager.create_table("manga_set", data=vectorized_mangas.to_dict(orient='records'))
 
-    reviewmatrix = ratings.pivot(
-        index="user id", columns="manga id", values="rating").fillna(0)
+    query_vector = vectorized_mangas.loc[vectorized_mangas['title'] == title, 'vector'].iloc[0]
+    result = db_manager.search("manga_set", query_vector, limit=size)
+    
+    # Construct return with title and manga ID sorted by distance
+    sorted_results = result.sort_values(by='_distance')  # Sort by distance
+    return_list = [{'title': row['title'], 'manga_id': row['id']} for _, row in sorted_results.iterrows()]
 
-    # SVD
-    matrix = reviewmatrix.values
-    u, s, vh = np.linalg.svd(matrix, full_matrices=False)
+    print(return_list)
+    
+test_main(title="Rebirth Of The Immortal Venerable", size=10)
 
-    vectors = np.rot90(np.fliplr(vh))
-    print(vectors.shape)
-
-    # Metadata
-    mangas = pd.read_csv('./manga-data/mangas.csv', header=0,
-                         names=["manga id", "title", "genres"])
-    mangas = mangas[mangas['manga id'].isin(reviewmatrix.columns)]
-
-    for i in range(len(mangas)):
-        data.append({"id": mangas.iloc[i]["manga id"], "title": mangas.iloc[i]
-                    ['title'], "vector": vectors[i], "genre": mangas.iloc[i]['genres']})
-    print(pd.DataFrame(data))
-
-    # Connect to LanceDB
-
-    db = lancedb.connect("./data/test-db")
-    try:
-        main.table = db.create_table("manga_set", data=data)
-    except:
-        main.table = db.open_table("manga_set")
-
-    print(get_recommendations("Moana (2016)"))
-    print(get_recommendations("Rogue One: A Star Wars Story (2016)"))
